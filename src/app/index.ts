098d9ecc578df9dd4896e27d803667eb50f2c679
jiangruomei <173887979@qq.com>
2022-05-08 08:05:15 +0800

    路由：Router

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import postRouter from '../post/post.router';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(postRouter);
 
 export default app;
---
+++ src/post/post.router.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import * as postController from './post.controller';
+
+const router = express.Router();
+
+router.get('/posts', postController.index);
+
+export default router;
