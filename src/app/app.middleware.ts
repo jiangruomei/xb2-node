1a3b0e7beda72428ee562a3c102dd17419ebb135
jiangruomei <173887979@qq.com>
2022-05-11 04:18:08 +0800

    利用数据之间的关系

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -16,6 +16,7 @@ export const defaultErrorHandler = (
   next: NextFunction,
 ) => {
   let statusCode: number, message: string;
+  console.log(error.message);
 
   switch (error.message) {
     default:
