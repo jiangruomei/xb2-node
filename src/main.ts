ebb7ec7788467d1009005f38f900d4ac921b5b18
jiangruomei <173887979@qq.com>
2022-05-10 21:50:14 +0800

    安装驱动连接数据仓库并测试

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import app from './app/index';
 import { APP_PORT } from './app/app.config';
+import { connection } from './app/database/mysql';
 
 app.listen(APP_PORT, () => {
   console.log('服务已经启动！');
+});
+
+connection.connect(error => {
+  if (error) {
+    console.log('连接数据服务失败：', error.message);
+    return;
+  }
+
+  console.log('成功连接数据服务 ~~');
 });
